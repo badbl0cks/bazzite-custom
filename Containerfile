3fab4d535c0a3d93a7ce8d30a9ad9b4fcc809d7c
badbl0cks <4161747+badbl0cks@users.noreply.github.com>
2024-10-30 13:33:31 -0700

    Move group definitions into build.sh with other pkgs, and use gh actions to replace groupinstall with install <pkgs>

--- Containerfile
+++ Containerfile
@@ -50,8 +50,6 @@ FROM ghcr.io/ublue-os/${SOURCE_IMAGE}${SOURCE_SUFFIX}:${FEDORA_VERSION}
 
 COPY build.sh /tmp/build.sh
 
-ARG GROUP_PKGS
-
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
     ostree container commit
--- build.sh
+++ build.sh
@@ -39,10 +39,8 @@ rpm-ostree install \
         htop \
         glances
 
-# from dnf groups (specify in build.yml)
-if [ -n "$GROUP_PKGS"]; then
-    rpm-ostree install $GROUP_PKGS
-fi
+# from dnf groups (gh actions will translate this to rpm-ostree install <pkgs>)
+rpm-ostree groupinstall "C Development Tools and Libraries" "Development Tools"
 
 # from rpmfusion
 rpm-ostree install vlc
